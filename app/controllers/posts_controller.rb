327fd5b192cbc3d5c4e0212b737e98f3d915721b
lieby weintraub <liebahinda@gmail.com>
2019-07-29 15:40:23 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,12 @@ class PostsController < ApplicationController
 if !params[:author].blank?
   @posts = Post.by_author(params[:author])
 elsif !params[:date].blank?
-    if params[:date] == "Today"
-      @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+        if params[:date] == "Today"
+      @posts = Post.from_today
     else
-      @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      @posts = Post.old_news
     end
+
   else
     # if no filters are applied, show all posts
     @posts = Post.all
